7183e855bbde2fcba68fb90b427cf509ac16206d
alex <alexmbcdo@gmail.com>
2022-11-07 22:59:07 -0300

    aula parte 2 - dia 07/11

--- src/capitulo8/Cap8_Lab2.java
+++ src/capitulo8/Cap8_Lab2.java
@@ -1,9 +1,17 @@
 package capitulo8;
 
 public class Cap8_Lab2 {
-
     public static void main(String[] args) {
-
-        
+        if(args.length > 0){
+            int soma = 0;
+            for (int i = 0; i < args.length; i++) {
+                soma += Integer.parseInt(args[i]);
+            }
+            int media = (soma / args.length);
+            System.out.println("A média das idades é de: " + media);
+        }
+        else{
+            System.out.println("Entre com valores válidos para as idades.");
+        }
     }
 }
