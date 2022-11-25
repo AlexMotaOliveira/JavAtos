7d48473ce9826fcca75416b5093c7004d97e84f5
LindaMoraes <lindadotto@yahoo.com.br>
2022-11-24 22:18:38 -0300

    Implementando swagger no método Post de AlunoController

--- cursos/src/main/java/br/com/javatos/cursos/controller/CursoController.java
+++ cursos/src/main/java/br/com/javatos/cursos/controller/CursoController.java
@@ -3,6 +3,7 @@ package br.com.javatos.cursos.controller;
 import br.com.javatos.cursos.model.Curso;
 import br.com.javatos.cursos.repository.CursoRepository;
 import br.com.javatos.cursos.service.CursoService;
+import io.swagger.annotations.ApiOperation;
 import jdk.dynalink.linker.LinkerServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class CursoController {
 
     private final CursoService cursoService;
 
+    @ApiOperation(value = "salvar um Curso")
     @PostMapping
     public Curso salvar (@RequestBody Curso curso){
         return cursoService.salvar(curso);
