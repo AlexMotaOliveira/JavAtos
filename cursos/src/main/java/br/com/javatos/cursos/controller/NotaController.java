e996fbeab1409a7fae0a622302fffd0fdab7007e
alex <alexmbcdo@gmail.com>
2022-11-24 21:34:29 -0300

    implementação do swagger no metodo Post de NotaController

--- cursos/src/main/java/br/com/javatos/cursos/controller/NotaController.java
+++ cursos/src/main/java/br/com/javatos/cursos/controller/NotaController.java
@@ -2,6 +2,8 @@ package br.com.javatos.cursos.controller;
 
 import br.com.javatos.cursos.model.Nota;
 import br.com.javatos.cursos.service.NotaService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,10 @@ public class NotaController {
     private final NotaService notaService;
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "criado"),
+            @ApiResponse(code = 500, message = "falha ao conectar com o banco de dados")
+    })
     public Nota salvar(@RequestBody Nota nota){
         return notaService.salvar(nota);
     }
