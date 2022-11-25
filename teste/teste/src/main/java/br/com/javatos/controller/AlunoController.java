860b120e9491af8b31003765288c450b6ed829cd
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2022-11-24 22:46:45 -0300

    feat: add logger info.
    Adicionado logger info do aluno salvo.

--- teste/teste/src/main/java/br/com/javatos/controller/AlunoController.java
+++ teste/teste/src/main/java/br/com/javatos/controller/AlunoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class AlunoController {
@@ -18,13 +19,16 @@ public class AlunoController {
     @Autowired
     public AlunoService alunoService;
 
+    private static Logger logger = Logger.getLogger(String.valueOf(AlunoController.class));
     @PostMapping("/aluno") // metodo e recurso = URI
     public Aluno salvar(@RequestBody Aluno aluno) {
+        logger.info("Salvando aluno: " + aluno);
         return alunoService.salvar(aluno);
     }
 
     @GetMapping(value = "/aluno", consumes = MediaType.ALL_VALUE)
     public List<Aluno> buscarTodosAlunos(){
+
         return  alunoService.buscarTodosAlunos();
     }
 
