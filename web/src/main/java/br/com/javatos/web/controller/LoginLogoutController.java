74175d83f21dabe77dc0fa5a8229dfeec274ad88
alex <alexmbcdo@gmail.com>
2022-12-02 22:51:10 -0300

    aula parte 2 - dia 01/12

---
+++ web/src/main/java/br/com/javatos/web/controller/LoginLogoutController.java
@@ -0,0 +1,13 @@
+package br.com.javatos.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginLogoutController {
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+}
