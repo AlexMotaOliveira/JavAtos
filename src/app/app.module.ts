b0cbbfff11d65419a1210a2062c6e7dd221899dc
Alex Mota Brito Cezar de Oliveira <alexmbcdo@gmail.com>
2023-01-06 18:10:11 -0300

    04-01-2023 part2 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
