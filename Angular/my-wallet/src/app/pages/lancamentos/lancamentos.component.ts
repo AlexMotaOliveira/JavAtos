879bf1865eec19897c86d667c8b90159fa6ed64f
Alex Mota Brito Cezar de Oliveira <alexmbcdo@gmail.com>
2023-01-16 22:44:36 -0300

    16-01-2023 -parte 2 / add elementos na tabela do my-wallet

--- Angular/my-wallet/src/app/pages/lancamentos/lancamentos.component.ts
+++ Angular/my-wallet/src/app/pages/lancamentos/lancamentos.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class LancamentosComponent {
 
+  lista:any [] = [
+    {code: 1,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 2,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 3,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 4,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 5,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 6,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 7,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 8,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 9,	name:'lapis',	category:'escolar',	price: 1},
+    {code: 10,	name:'lapis',	category:'escolar',	price: 1}
+  ];
+  statuses:any []= ['escolar','vesturario'];
+
+  onRowEditInit(product:any){
+
+  }
+
+  onRowEditSave(product:any){
+
+  }
+
+  onRowEditCancel(product:any, ri:any){
+
+  }
+
+  deletar(code:number){
+      this.lista = this.lista.filter(item => item.code !== code);
+  }
+
+
 }
--- Angular/my-wallet/src/app/pages/pages.module.ts
+++ Angular/my-wallet/src/app/pages/pages.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LancamentosComponent } from './lancamentos/lancamentos.component';
 import { CadastroDeLancamentosComponent } from './cadastro-de-lancamentos/cadastro-de-lancamentos.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import {TooltipModule} from 'primeng/tooltip';
 
 
 
@@ -11,7 +17,13 @@ import { CadastroDeLancamentosComponent } from './cadastro-de-lancamentos/cadast
     CadastroDeLancamentosComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    TableModule,
+    DropdownModule,
+    TooltipModule
   ],
   exports:[
     LancamentosComponent,
